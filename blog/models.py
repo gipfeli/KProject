3d7772e0f4ff3b5f161e11372c55b9c7f71c8c85
gipfeli <nguyenchanhdat@gmail.com>
2016-10-27 00:44:53 +0200

    timezone.now can get from django.utils library

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.utils import timezone
+
 # Create your models here.
 
 class Post(models.Model):
@@ -11,8 +13,8 @@ class Post(models.Model):
 	created_date = models.DateTimeField(
 		default=timezone.now)
 	published_date = models.DateTimeField(
-		blank=TRUE, 
-		null=TRUE)
+		blank=True, 
+		null=True)
 
 	def publish(self):
 		self.published_date = timezone.now()
