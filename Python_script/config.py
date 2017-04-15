6ba8ade83eb8fce822050beaa5652b4f8805ce00
gipfeli <nguyenchanhdat@gmail.com>
2017-04-15 12:59:01 +0200

    add more info to table

--- Python_script/config.py
+++ Python_script/config.py
@@ -1,6 +1,6 @@
 from configparser import ConfigParser
 
-def config(filename='database.ini', section='postgresql'):
+def connection(filename='database.ini', section='postgresql'):
     parser = ConfigParser()
     parser.read(filename)
 
--- Python_script/xmlparser.py
+++ Python_script/xmlparser.py
@@ -5,6 +5,7 @@ Created on Mon Mar 13 08:49:42 2017
 
 @author: Gipfeli
 """
+from config import connection
 
 from bs4 import BeautifulSoup 
 import requests
@@ -12,8 +13,8 @@ import psycopg2
 
 # XML-Flie should be in same folder like py-File
 def getdatafromlocal():
-    BeautifulSoup(open("ch.ofac.ca.covercard.CaValidationHorizontale.xml", "r", encoding="windows-1252"),"lxml")
-    return;
+    soup = BeautifulSoup(open("ch.ofac.ca.covercard.CaValidationHorizontale.xml", "r", encoding="windows-1252"),"lxml")
+    return soup;
 
 # Get XML file directly from URL: http://blabla.com/asd=xxxxxxxxx, where xxxxxxxxx is KK-Nr (get though input or a given list)
 def getdatafromURL():
@@ -28,34 +29,84 @@ def getdatafromURL():
 
     return soup;
 
+# Prepare data
+def preparedata():
+    soup = getdatafromlocal()
+    data = {
+       "KK-Nummer": soup.client.attrs['insuredpersonnumber'],
+       "AHV-Nummer": soup.client.attrs['cardholderidentifier'],
+       "Vorname": soup.find(name='first-name').string,
+       "Nachname": soup.find(name='last-name').string,
+       "Geschlecht": soup.find("ns1:entity").attrs['sex'],
+       "Geburtstag": soup.find(name='birth-date').string,
+       "Adresse": soup.find(name='street').string,
+       "PLZ": soup.find(name="zip").string
+       }
+    return data;
+    
+# Prepare SQL INSERT for table, Patient
+def insertpatient():
+    
+    sql =   """
+            INSERT INTO patient (vorname,nachname,geschlecht,geburtstag,contact_id,kk_nummer,ahv_nummer) 
+            VALUES (%s,%s,%s,%s,%s,%s,%s) RETURNING patient_id
+            """
+    return sql;
+
+# Prepare SQL INSERT for table, Adressenbuch
+def insertaddress():
+    
+    sql =   """
+            INSERT INTO adressebuch (adresse, plz) 
+            VALUES (%s,%s) RETURNING contact_id
+            """
+    return sql;
+
+# Commit the data into database
+def connect():
+    conn = None
+    try:
+        params = connection()
+
+        print('Connection to the Postgresql database....')
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+
+        print('PostgreSQL database Version:')
+        cur.execute('SELECT version()')
+        db_version = cur.fetchone()
+        print(db_version)
+        
+        cur.execute(sql1, (data['Adresse'], data['PLZ']))
+        contact_id = cur.fetchone()
+        
+        cur.execute(sql2, (data['Vorname'],data['Nachname'],data['Geschlecht'], 
+                               data['Geburtstag'],contact_id, data['KK-Nummer'], data['AHV-Nummer']))
+        patient_id = cur.fetchone()
+        conn.commit()
+        
+        print('Patient ID: ', patient_id)
+        
+        print('Contact ID: ', contact_id)
+        
+        cur.close()
+
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
 ###############################################################################
 
-soup = getdatafromURL()
-
-data = {
-   "KK-Nummer": soup.client.attrs['assure-id'],
-   "AHV-Nummer": soup.client.attrs['cardholderidentifier'],
-   "Vorname": soup.find(name='first-name').string,
-   "Nachname": soup.find(name='last-name').string,
-   "Geburtstag": soup.find(name='birth-date').string,
-   "Adresse": soup.find(name='street').string,
-   "PLZ": soup.find(name="zip").string
-        }
-
-sql =   """
-        INSERT INTO test080317.testdb (first,last,kkv) 
-        VALUES (%s,%s,%s) RETURNING test080317.testdb.id
-        """
-data = getdatafromURL()
-
-# Connect to database using config function above
-# TODO: use configparser to put login info in separated file
-conn = psycopg2.connect(host="localhost", port=5432, dbname='Gipfeli', user='Gipfeli', password='')
-print('Connected to database')
-# Create a cursor
-cur = conn.cursor()
-# Insert the data into table: patient
-# and request the patient_id
-id = cur.execute(sql, (data['Nachname'],data['Vorname'],data['KK-Nummer']))
-# commit the changes to the database
-conn.commit()
+if __name__ == '__main__':
+    
+    data = preparedata()
+    print(data)
+    
+    sql1 = insertaddress()
+    sql2 = insertpatient()
+    print(sql2)
+    
+    connect()
