57a5e0efa31e5a4c01ac68a91328331c3fd9f772
chanhdat <nguyenchanhdat@gmail.com>
2017-09-06 14:10:32 +0200

    Add search function

---
+++ Python_script/searchPatient.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  6 02:55:46 2017
+
+@author: Gipfeli
+"""
+
+from config import connection
+
+import psycopg2
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def option():
+    print("Search function: type '1' for names, type '2' for KK-Nr")
+    
+
+
+def connect():
+    conn = None
+    try:
+        params = connection()
+
+        
+        logger.debug('Connect to the Postgresql database....')
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+
+        if key.isdecimal():
+            sql = searchforKK()
+            logger.debug(sql)
+            cur.execute(sql, (var,))
+        else:
+            sql = searchforName()
+            logger.debug(sql)
+            cur.execute(sql, (var, var))
+        print(cur.fetchall())
+        
+        conn.commit()
+        
+        cur.close()
+
+    except(Exception, psycopg2.DatabaseError) as error:
+        logger.error(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            logger.debug('Database connection closed.') 
+            
+def searchforName():
+    sql_name = """
+              SELECT patient_id, vorname || ' ' || nachname AS fullname, kk_nummer
+              FROM patient
+              WHERE vorname ILIKE (%s) OR nachname ILIKE (%s)
+              LIMIT 25;
+              """
+    return sql_name;
+
+def searchforKK():
+    sql_nummer = """
+              SELECT patient_id, vorname || ' ' || nachname AS fullname, kk_nummer
+              FROM patient
+              WHERE kk_nummer ILIKE (%s)
+              LIMIT 25;
+              """
+    return sql_nummer;
+                
+            
+###############################################################################
+
+if __name__ == '__main__':
+
+    key = input('Search keywords: ')
+    var = '%' + key + '%'
+    
+    connect()
+
+
+      
